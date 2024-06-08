de04226b9421e521c037921a08a2d6bca3347611
andrebettencourt7 <andreluispb10@gmail.com>
2024-06-08 16:54:02 -0300

    feat:created-dash-page

--- App.js
+++ App.js
@@ -1,5 +1,25 @@
+import { ScrollView, View } from "react-native";
+import Card from "./src/type-weather/pages/dash/components/card";
 import Search from "./src/type-weather/pages/search";
+import WeatherDetail from "./src/type-weather/pages/dash/components/weather-detail";
+import NextDays from "./src/type-weather/pages/dash/components/next-days";
 
 export default function App() {
-  return <Search />;
+  return (
+  <ScrollView 
+  contentContainerStyle={{
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center", 
+    gap:20
+
+  }}
+  style={{ backgroundColor: '#13131A'}}>
+    <Card />
+    <WeatherDetail/>
+    <NextDays/>
+  </ScrollView>
+  
+  )
+
 }
